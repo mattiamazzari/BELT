519e4c4b6550be0ed5d8ca623854ac88d6b08131
Mazzarimovic22 <mattiamazzari@gmail.com>
2023-08-28 10:57:15 +0200

    feat: extended to multiclass classification

--- belt_nlp/bert_with_pooling.py
+++ belt_nlp/bert_with_pooling.py
@@ -35,6 +35,7 @@ class BertClassifierWithPooling(BertClassifier):
         chunk_size: int,
         stride: int,
         minimal_chunk_length: int,
+        num_classes: int, # Added num_classes
         pooling_strategy: str = "mean",
         maximal_text_length: Optional[int] = None,
         tokenizer: Optional[PreTrainedTokenizerBase] = None,
@@ -53,6 +54,8 @@ class BertClassifierWithPooling(BertClassifier):
             device,
             many_gpus,
         )
+        
+        self.num_classes = num_classes
 
         self.chunk_size = chunk_size
         self.stride = stride
